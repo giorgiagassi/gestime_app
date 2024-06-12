e0556a7eae3dc1053f8320b3c75f9b8fc285da67
gassig <giorgiagassi@gmail.com>
2024-06-12 16:25:35 +0200

    fix

--- src/app/pages/timbra-new/timbra-new.module.ts
+++ src/app/pages/timbra-new/timbra-new.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
@@ -14,6 +14,7 @@ import { TimbraNewPage } from './timbra-new.page';
         FormsModule,
         IonicModule,
         TimbraNewPageRoutingModule,
+        NgOptimizedImage,
     ],
   declarations: [TimbraNewPage]
 })
